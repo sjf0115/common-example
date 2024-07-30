6598fd3fa22a9f71b06fef3560b90dc380a08c55
sjf0115 <1203745031@qq.com>
2024-07-30 22:59:30 +0800

    MOD: SQLUnionQuery

--- druid-parser/src/main/java/com/example/druid/parser/mysql/MySQLSelectParser.java
+++ druid-parser/src/main/java/com/example/druid/parser/mysql/MySQLSelectParser.java
@@ -1,9 +1,17 @@
 package com.example.druid.parser.mysql;
 
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLName;
+import com.alibaba.druid.sql.ast.SQLOrderBy;
+import com.alibaba.druid.sql.ast.expr.SQLCharExpr;
+import com.alibaba.druid.sql.ast.expr.SQLListExpr;
 import com.alibaba.druid.sql.ast.statement.*;
+import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlSelectQueryBlock;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlSelectParser;
 import com.example.druid.parser.bean.MySQLSelect;
 import com.example.druid.parser.core.SelectSQLParser;
+
+import java.util.List;
 
 /**
  * 功能：MySQLSelectParser
@@ -21,10 +29,59 @@ public class MySQLSelectParser implements SelectSQLParser<MySQLSelect> {
         SQLSelectQuery query = select.getQuery();
         if (query instanceof SQLSelectQueryBlock) {
             // 对应单表查询
+            MySqlSelectQueryBlock queryBlock = (MySqlSelectQueryBlock) query;
+            SQLTableSource from = queryBlock.getFrom();
+
+            List<SQLSelectItem> selectList = queryBlock.getSelectList();
+            for (SQLSelectItem item : selectList) {
+
+            }
+
+            if(from instanceof SQLExprTableSource) {
+                //
+                SQLExprTableSource tableSource = (SQLExprTableSource) from;
+                String tableName = tableSource.getTableName();
+            } else if (from instanceof SQLLateralViewTableSource) {
+                //
+                SQLLateralViewTableSource lateralViewTableSource = (SQLLateralViewTableSource) from;
+                SQLTableSource tableSource = lateralViewTableSource.getTableSource();
+            } else if (from instanceof SQLValuesTableSource) {
+                //
+                SQLValuesTableSource valuesTableSource = (SQLValuesTableSource) from;
+                String tableAlias = valuesTableSource.getAlias();
+                System.out.println("TableAlias: " + tableAlias);
+                List<SQLListExpr> values = valuesTableSource.getValues();
+                for (SQLListExpr listExpr : values) {
+                    List<SQLExpr> items = listExpr.getItems();
+                    for (SQLExpr expr : items) {
+                        if (expr instanceof SQLCharExpr) {
+                            SQLCharExpr charExpr = (SQLCharExpr) expr;
+                            Object value = charExpr.getValue();
+                            System.out.println("Value: " + value);
+                        }
+                    }
+                }
+                List<SQLName> columns = valuesTableSource.getColumns();
+                for (SQLName name : columns) {
+                    System.out.println("Name: " + name.getSimpleName());
+                }
+            }
             System.out.println("对应单表查询");
         } else if (query instanceof SQLUnionQuery) {
             // 对应 UNION 查询
             System.out.println("对应 UNION 查询");
+            SQLUnionQuery unionQuery = (SQLUnionQuery) query;
+            SQLUnionOperator operator = unionQuery.getOperator();
+            String operatorName = operator.name();
+            System.out.println("Operator: " + operatorName);
+
+            SQLOrderBy orderBy = unionQuery.getOrderBy();
+            List<SQLSelectOrderByItem> items = orderBy.getItems();
+
+            List<SQLSelectQuery> relations = unionQuery.getRelations();
+            for (SQLSelectQuery selectQuery : relations) {
+
+            }
         } else if (query instanceof SQLValuesQuery) {
             // 对应 Values 查询
             System.out.println("对应 Values 查询");
--- druid-parser/src/test/java/com/example/druid/parser/mysql/SelectSQLTest.java
+++ druid-parser/src/test/java/com/example/druid/parser/mysql/SelectSQLTest.java
@@ -18,10 +18,37 @@ public class SelectSQLTest {
     private static final Gson gson = new GsonBuilder().create();
 
     @Test
-    public static void testSimpleSelect() {
+    public void testSimpleSelect() {
         MySQLParserFactory factory = new MySQLParserFactory();
         MySQLSelectParser parser = (MySQLSelectParser)factory.selectSQLParser();
         String sql = "SELECT * FROM user_profile_user";
+        MySQLSelect select = parser.parse(sql);
+        System.out.println(gson.toJson(select));
+    }
+
+    @Test
+    public void testLateralViewSelect() {
+        MySQLParserFactory factory = new MySQLParserFactory();
+        MySQLSelectParser parser = (MySQLSelectParser)factory.selectSQLParser();
+        String sql = "SELECT tag['id'] AS id FROM user_profile_user lateral view explode(tags) tags as tag";
+        MySQLSelect select = parser.parse(sql);
+        System.out.println(gson.toJson(select));
+    }
+
+    @Test
+    public void testValuesSelect() {
+        MySQLParserFactory factory = new MySQLParserFactory();
+        MySQLSelectParser parser = (MySQLSelectParser)factory.selectSQLParser();
+        String sql = "SELECT id, name FROM Values ('1', 'Lucy'),('2', 'Lily') t(id, name)";
+        MySQLSelect select = parser.parse(sql);
+        System.out.println(gson.toJson(select));
+    }
+
+    @Test
+    public void testUnionQuerySelect() {
+        MySQLParserFactory factory = new MySQLParserFactory();
+        MySQLSelectParser parser = (MySQLSelectParser)factory.selectSQLParser();
+        String sql = "SELECT id, name FROM user_profile_behavior UNION ALL SELECT id, name AS id FROM user_profile_label";
         MySQLSelect select = parser.parse(sql);
         System.out.println(gson.toJson(select));
     }
